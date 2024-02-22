88eb8fd983e9cd39849a4ce0f05007f382fc32a1
Reilly Shofner <reilly.shofner@gmail.com>
2024-02-21 18:30:06 -0800

    complete phase-1-control-flow-lab

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  const freeSampleMsg = 'This one is on me!';
+  const twentyDollarMsg = 'That will be twenty bucks.';
+  const thirtyDollarMsg = 'I will gladly take your thirty bucks.';
+  const notAllowedMsg = 'No can do.';
+
+  if (distance <= 400) {
+      return freeSampleMsg;
+  }  
+  else if (distance <= 2000) {
+    return twentyDollarMsg;
+  }
+  else if (distance <= 2500) {
+    return thirtyDollarMsg;
+  }
+  else if (distance > 2500) {
+    return notAllowedMsg;
+  }
+}
+function ternaryCheckCity(city){
+  return city === 'NYC' ? "Ok, sounds good." : "No go.";
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){  
+  switch (tip) {
+    case 'generous' :
+      return 'Thank you so much.';
+      break;
+      case 'not as generous' :
+        return 'Thank you.';
+        break;
+        default:
+          return 'Bye.';
+          break;
+  }
 }
